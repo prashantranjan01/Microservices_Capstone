398675ef1f88d1414190b3d328bb8223f8416b1a
Rohan Sharma <iamrohan29@gmail.com>
2025-05-26 00:48:58 +0530

    Added Data SQL

---
+++ config/sql/subcategory_insert.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM ecommerce.sub_category
+LIMIT 0, 1000
+
+-- Date: 2025-05-26 00:48
+*/
+INSERT INTO `` (`id`,`created_at`,`created_by`,`title`,`updated_at`,`category_id`) VALUES ('1fe57503-4d1c-4fd1-86c1-174f0ce99d28','2025-05-02 02:38:14.139143','7f69322d-af1c-424b-91b7-3db73e2a52d0','Fresh Fruits','2025-05-02 02:38:14.139143','7b47f95f-05ab-463c-85ab-82193a853fdf');
+INSERT INTO `` (`id`,`created_at`,`created_by`,`title`,`updated_at`,`category_id`) VALUES ('d970088d-d728-45b1-bad1-91ffc3d4088d','2025-05-24 23:57:15.719718','7f69322d-af1c-424b-91b7-3db73e2a52d0','Frozen Veg','2025-05-24 23:57:15.719718','7b47f95f-05ab-463c-85ab-82193a853fdf');
+INSERT INTO `` (`id`,`created_at`,`created_by`,`title`,`updated_at`,`category_id`) VALUES ('f4dca29e-60d0-4564-9299-b45c0fbe1a50','2025-05-24 23:56:57.590670','7f69322d-af1c-424b-91b7-3db73e2a52d0','Fresh Vegetables','2025-05-24 23:56:57.590670','7b47f95f-05ab-463c-85ab-82193a853fdf');
